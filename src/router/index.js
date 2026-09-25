26d97bf3a1ad3de4c61df90ad9e70889e494de85
resthu-mandala <resthumandala7@gmail.com>
2026-09-25 07:46:54 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../pages/LandingPage.vue'
 import ProductPage from '../pages/ProductPage.vue'
 import AboutPage from '../pages/AboutPage.vue'
+import ContactPage from '@/pages/ContactPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,8 @@ const router = createRouter({
     { path: '/', name: 'Landing', component: LandingPage },
     { path: '/product', name: 'Product', component: ProductPage },
     { path: '/about', name: 'About', component: AboutPage },
+    { path: '/contact', name: 'Contact', component: ContactPage },
+    
   ],
 })
 export default router
