2c8db13606e83419136176eeb56d3b18e96c9995
resthu-mandala <resthumandala7@gmail.com>
2026-09-25 07:42:38 +0700

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
  <nav>
- <h2>OBUD MERCH STORE</h2>
+ <h2>OBUD OFICIAL MERCH STORE</h2>
  <RouterLink to="/">Landing</RouterLink> |
  <RouterLink to="/product">Product</RouterLink> |
  <RouterLink to="/about">About</RouterLink> |
